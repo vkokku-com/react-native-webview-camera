6f398ac78bacd0f97e8b99ae357b64d02bf2ecb5
Venkata Saikiran Kokku <vkokku@paypalcorp.com>
2017-04-18 18:23:47 +0530

    added all files

---
+++ simpleForm/index.ios.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  WebView
+} from 'react-native';
+
+export default class simpleForm extends Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'http://localhost:9000/'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('simpleForm', () => simpleForm);
